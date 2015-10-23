c8d1c53ae53363de6a42f9e4d086ed0e53cf61e3
Tomas Kacur <tomas.kacur@keboola.com>
2015-10-23 18:17:25 +0200

    feat(upload): handle failed reques, post request

--- src/upload.py
+++ src/upload.py
@@ -2,17 +2,26 @@ import httplib2 # pip install httplib2
 import json
 
 connectionIndexUrl = 'https://connection.keboola.com/v2/storage'
+http = httplib2.Http()
 
 def parseResponse(response):
+    header = response[0]
+    status = int(header['status'])
+    if status not in range(200,300):
+        raise Exception('http request failed')
     body = json.loads(response[1])
-    result = { 'header': response[0], 'body': body}
+    result = { 'header': header, 'body': body}
     return result
 
 def getRequest(url):
-    h = httplib2.Http()
-    response = h.request(url, 'GET')
+    response = http.request(url, 'GET')
     return parseResponse(response)
 
+def postRequest(url, body, token):
+    headers = {'X-Storageapi-token': token}
+    body = json.dumps(body)
+    response = http.request(url, 'POST', headers=headers, body=body)
+    return parseResponse(response)
 
 def getComponenUri(componentId):
     body = loadConnectionIndex()['body']
@@ -23,7 +32,5 @@ def getComponenUri(componentId):
 def loadConnectionIndex():
     return getRequest(connectionIndexUrl)
 
-
-
-def uploadTask(componentId, params):
+def uploadTask(componentId, params, token):
     return 0
