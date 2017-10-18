41ce0721ca0628c4ed7b356a125f719ea3fd5aeb
Tomas Kacur <tomas.kacur@keboola.com>
2017-10-18 15:28:47 +0200

    fix deploy script: chmod +x and exit on first command fail

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -e
+
 docker pull quay.io/keboola/developer-portal-cli-v2:latest
 export REPOSITORY=`docker run --rm -e KBC_DEVELOPERPORTAL_USERNAME=$KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD=$KBC_DEVELOPERPORTAL_PASSWORD quay.io/keboola/developer-portal-cli-v2:latest ecr:get-repository keboola tde-exporter`
 docker tag keboola/tde-exporter:latest $REPOSITORY:$TRAVIS_TAG
